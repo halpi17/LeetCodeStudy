9f5e38f186eaf98366b3f2b4fe53a7bc0bd0a9a0
halpi17 <nishiyamaymyt@gmail.com>
2021-05-26 10:44:52 +0900

    add min max range

--- 002/reverseInteger.py
+++ 002/reverseInteger.py
@@ -7,6 +7,10 @@ class Solution:
       reverse_x = int(str(x)[::-1])
     if x < 0:
       reverse_x = -1 * int(str(-1*x)[::-1])
+    min_reverse_x = -2 ** 31
+    max_reverse_x = -2 ** 31 -1
+    if reverse_x in range(min_reverse_x, max_reverse_x):
+      return 0
     return reverse_x
 
 def main():
