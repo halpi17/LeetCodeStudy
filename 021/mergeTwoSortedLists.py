3e0097d2f78bc1bb50c733bf0be98fece6f9bc1e
halpi17 <nishiyamaymyt@gmail.com>
2021-06-17 15:58:23 +0900

    Add: Iterative solution

--- 021/mergeTwoSortedLists.py
+++ 021/mergeTwoSortedLists.py
@@ -5,15 +5,30 @@ class ListNode:
 
 
 class Solution:
+    # def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    #     if not l1:
+    #         return l2
+    #     if not l2:
+    #         return l1
+    #     if l1.val < l2.val:
+    #         l1.next = self.mergeTwoLists(l1.next, l2)
+    #         return l1
+    #     else:
+    #         l2.next = self.mergeTwoLists(l1, l2.next)
+    #         return l2
+
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1 or not l2:
-            return l1 or l2
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
+        dummy = tmp = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                tmp.next = l1
+                l1 = l1.next
+            else:
+                tmp.next = l2
+                l2 = l2.next
+            tmp = tmp.next
+        tmp.next = l1 or l2
+        return dummy.next
 
     def inputList(self, l1: ListNode, l2: ListNode):
         node1 = l1
