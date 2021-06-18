fdaae75cb2d682a5245b05e7da1747e62c057262
halpi17 <nishiyamaymyt@gmail.com>
2021-06-18 23:04:13 +0900

    Done. 28.Implement strStr()

---
+++ 028/implement_strStr.py
@@ -0,0 +1,22 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+def main():
+    haystack = "hello"
+    needle = "ll"
+    # haystack = "aaaaa"
+    # needle = "bba"
+    # haystack = ""
+    # needle = ""
+    solution = Solution()
+    print(f"Input: haystack = \"{haystack}\", needle = \"{needle}\"")
+    print(f"Output: {solution.strStr(haystack, needle)}")
+
+if __name__ == "__main__":
+    main()
