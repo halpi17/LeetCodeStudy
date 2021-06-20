5a49e257e4b728e2afb3a9864ba2492759ad226b
halpi17 <nishiyamaymyt@gmail.com>
2021-06-20 23:34:51 +0900

    Add:Solutions for O(n)

--- 053/maximumSubarray.py
+++ 053/maximumSubarray.py
@@ -2,15 +2,28 @@ from typing import List
 
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
+    # def maxSubArray(self, nums: List[int]) -> int:
+    #     max_sum = nums[0]
 
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                subarray_sum = sum(nums[i:i+j+1])
-                # print(f"i: {i}, i+j: {i+j}, subarray_sum = {subarray_sum}")
-                if max_sum < subarray_sum:
-                    max_sum = subarray_sum
+    #     for i in range(len(nums)):
+    #         for j in range(len(nums)):
+    #             subarray_sum = sum(nums[i:i+j+1])
+    #             # print(f"i: {i}, i+j: {i+j}, subarray_sum = {subarray_sum}")
+    #             if max_sum < subarray_sum:
+    #                 max_sum = subarray_sum
+
+    #     return max_sum
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        subarray_sum = max_sum = nums[0]
+
+        for num in nums[1:]:
+            if num < subarray_sum + num:
+                subarray_sum = subarray_sum + num
+            else:
+                subarray_sum = num
+            if max_sum < subarray_sum:
+                max_sum = subarray_sum
 
         return max_sum
 
